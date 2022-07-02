d445950649c43ec1ffbb35658eaaca8244f9c020
Sami <s.m.sami125@gmail.com>
2022-07-02 17:30:58 +0500

    mine block

--- blockchain.py
+++ blockchain.py
@@ -2,7 +2,6 @@ import hashlib
 import datetime
 import json
 from flask import Flask, jsonify
-
 #part1: Building a blockchain
 class Blockchain: 
   def __init__(self):
@@ -56,3 +55,29 @@ class Blockchain:
      return False
 
 #part2 mining our blockchain
+
+# Creating a flask server
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
+#mining our new block
+@app.route("/mine_block",methods= ["GET"])
+def mine_block():
+  previous_block= blockchain.get_previous_block()
+  previous_proof= previous_block["proof"]
+  proof= blockchain.proof_of_work(previous_proof)
+  previous_hash= blockchain.hash(previous_block)
+  block =blockchain.create_block(proof,previous_hash)
+  response = {
+    "message": "Congratulations, You just mined a block!",
+    **block
+  }
+  return jsonify(response),200
+
+@app.route("/get_chain",methods= ["GET"])
+def get_chain():  
+   response = {"chain": blockchain.chain,"length": len(blockchain.chain)}
+   return jsonify(response),200
